210af1b4ded55de24c8e40b81bd9b5edfc0c4826
Demalee <dev.demalee@gmail.com>
2026-02-04 20:50:35 +0300

    fix allow-unathourized issue in cloud build

--- app/main.py
+++ app/main.py
@@ -124,5 +124,5 @@ app.add_middleware(
 # ========== MAIN EXECUTION ==========
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8800))
     uvicorn.run(app, host="0.0.0.0", port=port)
